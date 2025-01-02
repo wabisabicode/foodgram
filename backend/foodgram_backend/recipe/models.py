51f5fe48cd73c72bf5721786918ee5737acf4622
wabisabicode <tretyakov@uni-mainz.de>
2025-01-02 22:53:34 +0100

    add Tag model

--- backend/foodgram_backend/recipe/models.py
+++ backend/foodgram_backend/recipe/models.py
@@ -2,12 +2,25 @@ from django.db import models
 
 
 class Ingredient(models.Model):
-    name = models.CharField(max_length=50)
-    measurement_unit = models.CharField(max_length=50)
+    name = models.CharField(max_length=50, verbose_name='Название')
+    measurement_unit = models.CharField(
+        max_length=50, verbose_name='Единицы измерения')
 
     class Meta:
-        verbose_name = 'Ingredient'
-        verbose_name_plural = 'Ingredients'
+        verbose_name = 'Ингредиент'
+        verbose_name_plural = 'Ингредиенты'
+
+    def __str__(self):
+        return self.name
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50, verbose_name='Название')
+    slug = models.SlugField(max_length=50, unique=True, verbose_name='Слаг')
+
+    class Meta:
+        verbose_name = 'Тег'
+        verbose_name_plural = 'Теги'
 
     def __str__(self):
         return self.name
