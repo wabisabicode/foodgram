1046a619b34731afac31249e5e0d5d80f599a32d
wabisabicode <tretyakov@uni-mainz.de>
2025-01-03 00:57:25 +0100

    add FGUser model

--- backend/foodgram_backend/users/models.py
+++ backend/foodgram_backend/users/models.py
@@ -1,3 +1,18 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class FGUser(AbstractUser):
+    email = models.EmailField(max_length=254, required=True)
+    first_name = models.CharField(max_length=150, required=True)
+    last_name = models.CharField(max_length=150, required=True)
+
+    username = models.CharField(unique=True, max_length=150)
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+        ordering = ('username',)
+
+    def __str__(self):
+        return self.username
