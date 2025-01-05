a8b14dc33cdc94b5fb658cff9b557719658d19e5
wabisabicode <tretyakov@uni-mainz.de>
2025-01-05 18:36:08 +0100

    fix line length in urls.py

--- backend/foodgram_backend/api/urls.py
+++ backend/foodgram_backend/api/urls.py
@@ -9,9 +9,10 @@ router_v1.register('users', UsersViewSet, basename='users')
 
 urlpatterns = [
     path('', include(router_v1.urls)),
-    path('users/me/', MeView.as_view(), name='user-me'),
-    path('users/me/avatar/', AvatarView.as_view(), name='user-avatar'),
-    path('users/set_password/', SetPasswordView.as_view(), name='user-set-password'),
+    path('users/me/', MeView.as_view(), name='users-me'),
+    path('users/me/avatar/', AvatarView.as_view(), name='users-avatar'),
+    path('users/set_password/', SetPasswordView.as_view(),
+         name='user-set-password'),
     path('auth/token/login/', TokenLoginView.as_view(), name='token-login'),
     path('auth/token/logout/', TokenLogoutView.as_view(), name='token-logout'),
 ]
