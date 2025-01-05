fe8d8543d6141ad74f1a5dd93c136f55ba1e0295
wabisabicode <tretyakov@uni-mainz.de>
2025-01-05 19:25:56 +0100

    import UserViewSet in urls.py

--- backend/foodgram_backend/api/urls.py
+++ backend/foodgram_backend/api/urls.py
@@ -1,18 +1,20 @@
 from django.urls import include, path
 from rest_framework.routers import SimpleRouter
 
+from .views import UserViewSet
+
 app_name = 'api'
 
 router_v1 = SimpleRouter()
 # makes /api/users/ and /api/users/{id}/:
-router_v1.register('users', UsersViewSet, basename='users')
+router_v1.register('users', UserViewSet, basename='users')
 
 urlpatterns = [
     path('', include(router_v1.urls)),
-    path('users/me/', MeView.as_view(), name='users-me'),
-    path('users/me/avatar/', AvatarView.as_view(), name='users-avatar'),
-    path('users/set_password/', SetPasswordView.as_view(),
-         name='user-set-password'),
-    path('auth/token/login/', TokenLoginView.as_view(), name='token-login'),
-    path('auth/token/logout/', TokenLogoutView.as_view(), name='token-logout'),
+    # path('users/me/', MeView.as_view(), name='users-me'),
+    # path('users/me/avatar/', AvatarView.as_view(), name='users-avatar'),
+    # path('users/set_password/', SetPasswordView.as_view(),
+    #      name='user-set-password'),
+    # path('auth/token/login/', TokenLoginView.as_view(), name='token-login'),
+    # path('auth/token/logout/', TokenLogoutView.as_view(), name='token-logout'),
 ]
