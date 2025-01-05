a8771aa6fcddfe6be519b5e3ab562afa2dfe4730
wabisabicode <tretyakov@uni-mainz.de>
2025-01-05 19:25:12 +0100

    add UserViewSet with get and post allowed

--- backend/foodgram_backend/api/views.py
+++ backend/foodgram_backend/api/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+# rest_framework.views import APIView
+from django.contrib.auth import get_user_model
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from .serializers import UserSerializer
+
+User = get_user_model()
+
+
+class UserViewSet(ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    http_method_names = ['get', 'post']
