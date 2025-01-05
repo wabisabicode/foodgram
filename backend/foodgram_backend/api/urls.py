589240d38778ccc33c0b476d33c11060a11c3c51
wabisabicode <tretyakov@uni-mainz.de>
2025-01-06 00:25:37 +0100

    add djoser urls

--- backend/foodgram_backend/api/urls.py
+++ backend/foodgram_backend/api/urls.py
@@ -10,7 +10,9 @@ router_v1 = SimpleRouter()
 router_v1.register('users', UserViewSet, basename='users')
 
 urlpatterns = [
-    path('', include(router_v1.urls)),
+    path('', include('djoser.urls')),
+    path('auth/', include('djoser.urls.authtoken')),
+    # path('', include(router_v1.urls)),
     # path('users/me/', MeView.as_view(), name='users-me'),
     # path('users/me/avatar/', AvatarView.as_view(), name='users-avatar'),
     # path('users/set_password/', SetPasswordView.as_view(),
