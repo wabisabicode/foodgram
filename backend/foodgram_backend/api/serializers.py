13f649a24a7906a16776a684248290a8d9f2efdd
wabisabicode <tretyakov@uni-mainz.de>
2025-01-05 19:24:47 +0100

    add simple UserSerializer

---
+++ backend/foodgram_backend/api/serializers.py
@@ -0,0 +1,13 @@
+from django.contrib.auth import get_user_model
+from rest_framework.serializers import ModelSerializer
+
+# from users.models import FGUser
+# from recipe.models import *
+
+User = get_user_model()
+
+
+class UserSerializer(ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
