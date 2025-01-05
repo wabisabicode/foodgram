f206efc1be5d69ac6f924169e2537bc53f70899a
wabisabicode <tretyakov@uni-mainz.de>
2025-01-05 01:45:04 +0100

    simplify urls.py for api

--- backend/foodgram_backend/api/urls.py
+++ backend/foodgram_backend/api/urls.py
@@ -7,23 +7,11 @@ router_v1 = SimpleRouter()
 # makes /api/users/ and /api/users/{id}/:
 router_v1.register('users', UsersViewSet, basename='users')
 
-me_patterns = [
-    path('', MeView.as_view(), name='user-me'),
-    path('avatar/', AvatarView.as_view(), name='user-avatar'),
-]
-
-password_patterns = [
-    path('', SetPasswordView.as_view(), name='user-set-password'),
-]
-
-auth_patterns = [
-    path('login/', TokenLoginView.as_view(), name='token-login'),
-    path('logout/', TokenLogoutView.as_view(), name='token-logout'),
-]
-
 urlpatterns = [
     path('', include(router_v1.urls)),
-    path('users/me/', include(me_patterns)),
-    path('users/set_password/', include(password_patterns)),
-    path('auth/token/', include(password_patterns)),
+    path('users/me/', MeView.as_view(), name='user-me'),
+    path('users/me/avatar/', AvatarView.as_view(), name='user-avatar'),
+    path('users/set_password/', SetPasswordView.as_view(), name='user-set-password'),
+    path('auth/token/login/', TokenLoginView.as_view(), name='token-login'),
+    path('auth/token/logout/', TokenLogoutView.as_view(), name='token-logout'),
 ]
