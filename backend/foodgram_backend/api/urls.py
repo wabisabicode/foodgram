65af243cbb3d6fc14086b43bb75bf3dce9582b70
wabisabicode <tretyakov@uni-mainz.de>
2025-01-06 22:40:24 +0100

    remove comments from urls.py

--- backend/foodgram_backend/api/urls.py
+++ backend/foodgram_backend/api/urls.py
@@ -11,13 +11,10 @@ router_v1.register('users', UserViewSet, basename='users')
 
 urlpatterns = [
     path('', include(router_v1.urls)),
-    # path('', include('djoser.urls')),
-    # path('auth/', include('djoser.urls.authtoken')),
     path('users/me/', MeViewSet, name='users-me'),
     # path('users/me/avatar/', AvatarView.as_view(), name='users-avatar'),
     # path('users/set_password/', SetPasswordView.as_view(),
     #      name='user-set-password'),
     path('auth/token/login/', TokenCreateView.as_view(), name='token-login'),
-    # path('auth/token/logout/', include('djoser.urls.authtoken')),
     path('auth/token/logout/', TokenLogoutView.as_view(), name='token-logout'),
 ]
