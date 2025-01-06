8ff497b6c36d69b7e70fa25076524fcf4f0086d1
wabisabicode <tretyakov@uni-mainz.de>
2025-01-06 23:42:40 +0100

    remove comments from serializers.py

--- backend/foodgram_backend/api/serializers.py
+++ backend/foodgram_backend/api/serializers.py
@@ -62,29 +62,8 @@ class SetPasswordSerializer(serializers.Serializer):
     current_password = serializers.CharField(
         required=True, style={'input_type': 'password'})
 
-# class TokenSerializer(serializer):
-#     class Meta:
-#         model = User
-#         fields = ('email', 'id', 'username',
-#                   'first_name', 'last_name', 'avatar')
-
 
 class TokenCreateSerializer(serializers.Serializer):
     email = serializers.EmailField(max_length=254, required=True)
     password = serializers.CharField(
         required=True, style={'input_type': 'password'})
-
-    # def __init__(self, *args, **kwargs):
-    #     super().__init__(*args, **kwargs)
-    #     self.user = None
-
-    # def validate(self, attrs):
-    #     password = attrs.get('password')
-    #     email = attrs.get('email')
-        # print(email)
-
-        # self.user = authenticate(email=email, password=password)
-
-        # if not self.user:
-        #     raise serializers.ValidationError('Invalid credentials')
-        # return attrs
