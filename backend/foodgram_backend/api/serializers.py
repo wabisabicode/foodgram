bbaf2ab1c2f78e43748e37855203a97110c2a2a5
wabisabicode <tretyakov@uni-mainz.de>
2025-01-06 14:55:20 +0100

    add CustomUserCreateSerializer

--- backend/foodgram_backend/api/serializers.py
+++ backend/foodgram_backend/api/serializers.py
@@ -1,21 +1,18 @@
 from django.contrib.auth import get_user_model
 from djoser.serializers import UserSerializer, UserCreateSerializer
-# from rest_framework.serializers import ModelSerializer
-
-# from users.models import FGUser
-# from recipe.models import *
 
 User = get_user_model()
+
+
+class CustomUserCreateSerializer(UserCreateSerializer):
+    class Meta:
+        model = User
+        fields = ('email', 'id', 'username',
+                  'first_name', 'last_name', 'password')
 
 
 class CustomUserSerializer(UserSerializer):
     class Meta:
         model = User
-        fields = ('email', 'id', 'username', 'first_name', 'last_name')
-
-# class UserSerializer(ModelSerializer):
-#     class Meta:
-#         model = User
-#         fields = ('email', 'id', 'username',
-#                   'first_name', 'last_name')#, 'avatar')
-#         extra_kwargs = {'password': {'write_only': True}}
+        fields = ('email', 'id', 'username',
+                  'first_name', 'last_name', 'avatar')
