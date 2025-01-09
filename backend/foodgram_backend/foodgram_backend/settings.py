497e2f4e47beeca2156719402cef1343d99ba094
wabisabicode <tretyakov@uni-mainz.de>
2025-01-09 23:59:22 +0100

    add django_filters to INSTALLED_APPS

--- backend/foodgram_backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/foodgram_backend/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
+    'django_filters',
     'djoser',
     'recipe.apps.RecipeConfig',
     'users.apps.UsersConfig',
