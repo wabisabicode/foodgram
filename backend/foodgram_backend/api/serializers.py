81d154ad11625df85244521dc52354e47f63d3fb
wabisabicode <tretyakov@uni-mainz.de>
2025-01-11 02:09:55 +0100

    fix line-too-long

--- backend/foodgram_backend/api/serializers.py
+++ backend/foodgram_backend/api/serializers.py
@@ -19,7 +19,8 @@ class Base64ImageField(serializers.ImageField):
             format, imgstr = data.split(';base64,')
             ext = format.split('/')[-1]
 
-            data = ContentFile(base64.b64decode(imgstr), name=generate_random_filename() + '.' + ext)
+            data = ContentFile(base64.b64decode(imgstr),
+                               name=generate_random_filename() + '.' + ext)
 
         return super().to_internal_value(data)
 
