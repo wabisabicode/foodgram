6e68e8ae6c48c5c2837555b207c5cbe021d7a85a
wabisabicode <tretyakov@uni-mainz.de>
2025-01-13 17:18:46 +0100

    add Favorite to admin

--- backend/foodgram_backend/recipe/admin.py
+++ backend/foodgram_backend/recipe/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import Ingredient, Recipe, Tag
+from .models import Ingredient, Recipe, Tag, Favorite
 
 admin.site.register(Ingredient)
 admin.site.register(Recipe)
 admin.site.register(Tag)
+admin.site.register(Favorite)
