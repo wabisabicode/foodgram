778cde8613255f4fcf9227ffc3dc8f0d66e716b0
wabisabicode <tretyakov@uni-mainz.de>
2025-01-15 18:11:59 +0100

    fix view function name in urls.py

--- backend/foodgram_backend/api/urls.py
+++ backend/foodgram_backend/api/urls.py
@@ -24,6 +24,6 @@ urlpatterns = [
     path('auth/token/login/', TokenCreateView.as_view(), name='token-login'),
     path('auth/token/logout/', TokenLogoutView.as_view(), name='token-logout'),
     path('recipes/download_shopping_list/',
-         download_shopping_cart.as_view(), name='download-shopping-list'),
+         download_shopping_cart, name='download-shopping-list'),
     path('', include(router_v1.urls)),
 ]
