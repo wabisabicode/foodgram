dbacd22d152fd01362a300acc4d4bbd7da03b434
wabisabicode <tretyakov@uni-mainz.de>
2025-01-16 15:49:34 +0100

    remove Favorite from admin panel

--- backend/foodgram_backend/recipe/admin.py
+++ backend/foodgram_backend/recipe/admin.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
 
-from .models import Ingredient, Recipe, Tag, Favorite
+from .models import Ingredient, Recipe, Tag
 from .models import RecipeTag, RecipeIngredient
 
 admin.site.register(Tag)
-admin.site.register(Favorite)
 
 
 class RecipeTagInline(admin.TabularInline):
