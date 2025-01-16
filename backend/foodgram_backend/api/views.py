d20deafc2618c3a201e1a27e2c37c0ef45937e16
wabisabicode <tretyakov@uni-mainz.de>
2025-01-16 13:33:43 +0100

    fix permission classes when downloading the shopping_cart

--- backend/foodgram_backend/api/views.py
+++ backend/foodgram_backend/api/views.py
@@ -12,7 +12,7 @@ from rest_framework.permissions import (
     AllowAny, IsAuthenticated, IsAuthenticatedOrReadOnly)
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 
 from .permissions import IsAuthorOrReadOnly
 from .serializers import CustomUserSerializer, CustomUserCreateSerializer
@@ -309,6 +309,7 @@ def shortURLRedirect(request, hash):
 
 
 @api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def download_shopping_cart(request):
     cart_recipes = Recipe.objects.filter(shopping_cart_items__user=request.user)
 
