fa7fb7ed15a83e48d8720770ca8488619beb9724
wabisabicode <tretyakov@uni-mainz.de>
2025-01-17 14:31:06 +0100

    fix last line-too-long in recipe/models.py

--- backend/foodgram_backend/recipe/models.py
+++ backend/foodgram_backend/recipe/models.py
@@ -117,7 +117,8 @@ class RecipeShortURL(models.Model):
 
 
 class Favorite(models.Model):
-    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name='favorites')
+    recipe = models.ForeignKey(
+        Recipe, on_delete=models.CASCADE, related_name='favorites')
     user = models.ForeignKey(FGUser, on_delete=models.CASCADE)
 
     class Meta:
