312459f3bf279100c775533acf019e5f24cc7a09
wabisabicode <tretyakov@uni-mainz.de>
2025-01-17 14:18:18 +0100

    fix isort in recipe app

--- backend/foodgram_backend/recipe/admin.py
+++ backend/foodgram_backend/recipe/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 
-from .models import Ingredient, Recipe, Tag
-from .models import RecipeTag, RecipeIngredient
+from .models import Ingredient, Recipe, RecipeIngredient, RecipeTag, Tag
 
 admin.site.register(Tag)
 
