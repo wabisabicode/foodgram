bd669af0d8239cfd7824ff7aad657ca524bb5baa
wabisabicode <tretyakov@uni-mainz.de>
2025-01-20 09:18:05 +0100

    fix short links redirection

--- backend/foodgram_backend/recipe/views.py
+++ backend/foodgram_backend/recipe/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import get_object_or_404, redirect
-from django.urls import reverse
 
 from .models import RecipeShortURL
 
 
 def redirect_from_short_url(request, hash):
     recipe_short_url = get_object_or_404(RecipeShortURL, hash=hash)
-    recipe_detail_url = reverse(
-        'api:recipes-detail', kwargs={'pk': recipe_short_url.recipe.pk})
-    return redirect(recipe_detail_url)
+    recipe_detail_path = f'/recipes/{recipe_short_url.recipe.pk}/'
+    recipe_full_url = request.build_absolute_uri(recipe_detail_path)
+
+    return redirect(recipe_full_url)
