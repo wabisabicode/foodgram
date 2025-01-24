629ede5d457adf78e590be2ac35ae1f27d9bb388
wabisabicode <tretyakov@uni-mainz.de>
2025-01-24 16:46:53 +0100

    add password change to the admin panel

--- backend/foodgram_backend/users/admin.py
+++ backend/foodgram_backend/users/admin.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import Group
 from rest_framework.authtoken.models import TokenProxy
 
 from .models import FGUser
 
 
-class FGUserAdmin(admin.ModelAdmin):
+class FGUserAdmin(UserAdmin):
     list_display = (
         'username',
         'email',
@@ -16,13 +17,20 @@ class FGUserAdmin(admin.ModelAdmin):
         'is_staff',
         'is_active',
         'is_superuser',
-        'last_login'
+        'last_login',
     )
     search_fields = (
         'username',
         'email'
     )
-    exclude = ('groups', 'user_permissions')
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'email')}),
+        ('Permissions', {
+            'fields': ('is_active', 'is_staff', 'is_superuser'),
+        }),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
 
 
 admin.site.register(FGUser, FGUserAdmin)
