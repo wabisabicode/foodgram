b72bfcd6339a81c4d27c7568e100cbb3243546ed
wabisabicode <tretyakov@uni-mainz.de>
2025-01-24 21:14:05 +0100

    translate Recipe App verbose name

--- backend/foodgram_backend/recipe/apps.py
+++ backend/foodgram_backend/recipe/apps.py
@@ -4,4 +4,4 @@ from django.apps import AppConfig
 class RecipeConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'recipe'
-    verbose_name = 'App recipe'
+    verbose_name = 'Приложение Рецепты'
