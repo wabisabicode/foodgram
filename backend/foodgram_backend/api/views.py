aba98381033a65f078ce6196c054dbd8cb76b016
wabisabicode <tretyakov@uni-mainz.de>
2025-01-25 17:01:29 +0100

    use ReadOnlyModelViewSet in TagList and IngredientList

--- backend/foodgram_backend/api/views.py
+++ backend/foodgram_backend/api/views.py
@@ -9,6 +9,7 @@ from rest_framework.permissions import (AllowAny, IsAuthenticated,
                                         IsAuthenticatedOrReadOnly)
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.viewsets import ReadOnlyModelViewSet
 
 from recipe.models import (Favorite, Ingredient, Recipe, RecipeIngredient,
                            RecipeShortURL, Tag)
@@ -146,17 +147,13 @@ class AvatarView(APIView):
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
-class TagListRetrieveViewSet(mixins.ListModelMixin,
-                             mixins.RetrieveModelMixin,
-                             viewsets.GenericViewSet):
+class TagListRetrieveViewSet(ReadOnlyModelViewSet):
     queryset = Tag.objects.all()
     serializer_class = TagReadSerializer
     pagination_class = None
 
 
-class IngredientListRetrieveViewSet(mixins.ListModelMixin,
-                                    mixins.RetrieveModelMixin,
-                                    viewsets.GenericViewSet):
+class IngredientListRetrieveViewSet(ReadOnlyModelViewSet):
     queryset = Ingredient.objects.all()
     serializer_class = IngredientSerializer
     filterset_class = IngredientFilter
