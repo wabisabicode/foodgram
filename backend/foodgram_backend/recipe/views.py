559f3e170e6a078f8c9abce8377b07c92128c4ad
wabisabicode <tretyakov@uni-mainz.de>
2025-01-26 19:23:10 +0100

    simplify short-link redirection

--- backend/foodgram_backend/recipe/views.py
+++ backend/foodgram_backend/recipe/views.py
@@ -5,7 +5,4 @@ from .models import RecipeShortURL
 
 def redirect_from_short_url(request, hash):
     recipe_short_url = get_object_or_404(RecipeShortURL, hash=hash)
-    recipe_detail_path = f'/recipes/{recipe_short_url.recipe.pk}/'
-    recipe_full_url = request.build_absolute_uri(recipe_detail_path)
-
-    return redirect(recipe_full_url)
+    return redirect('recipes-detail', pk=recipe_short_url.recipe.pk)
