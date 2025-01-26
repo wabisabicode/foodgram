54916370441ace55cd79b0da09bb1f3e533436c6
wabisabicode <tretyakov@uni-mainz.de>
2025-01-26 20:09:00 +0100

    refactor filtering by tags to use ModelMultipleChoiceFilter

--- backend/foodgram_backend/api/filters.py
+++ backend/foodgram_backend/api/filters.py
@@ -1,6 +1,6 @@
 from django_filters import rest_framework as filters
 
-from recipe.models import Ingredient, Recipe
+from recipe.models import Ingredient, Recipe, Tag
 
 
 class IngredientFilter(filters.FilterSet):
@@ -12,22 +12,19 @@ class IngredientFilter(filters.FilterSet):
 
 
 class RecipeFilter(filters.FilterSet):
-    tags = filters.CharFilter(method='filter_tags')
+    tags = filters.ModelMultipleChoiceFilter(
+        queryset=Tag.objects.all(),
+        field_name='tags__slug',
+        to_field_name='slug'
+    )
     is_favorited = filters.BooleanFilter(
         method='filter_favorites')
     is_in_shopping_cart = filters.BooleanFilter(
         method='filter_shopping_cart')
-    author = filters.NumberFilter(field_name='author__id')
 
     class Meta:
         model = Recipe
-        fields = ('tags', 'is_favorited', 'is_in_shopping_cart')
-
-    def filter_tags(self, queryset, name, value):
-        tags = self.request.query_params.getlist('tags', [])
-        if tags:
-            return queryset.filter(tags__slug__in=tags).distinct()
-        return queryset
+        fields = ('tags', 'is_favorited', 'is_in_shopping_cart', 'author')
 
     def filter_favorites(self, queryset, name, value):
         if value:
