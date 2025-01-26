d847b1faa6fa68b89f172159ce25d59fb029ea82
wabisabicode <tretyakov@uni-mainz.de>
2025-01-26 22:30:57 +0100

    introduce reverse dynamic loading of url

--- backend/foodgram_backend/recipe/views.py
+++ backend/foodgram_backend/recipe/views.py
@@ -1,15 +1,15 @@
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, redirect
+from django.urls import reverse
 
 from .models import RecipeShortURL
 
 
 def redirect_from_short_url(request, hash):
     recipe_short_url = get_object_or_404(RecipeShortURL, hash=hash)
-    return redirect(
-        'frontend-recipe-detail',
-        kwargs={'pk': recipe_short_url.recipe.pk}
-    )
+    recipe_url = reverse('frontend-recipe-detail',
+                         kwargs={'pk': recipe_short_url.recipe.pk})
+    return redirect(recipe_url)
 
 
 def placeholder_view(request, pk):
