d3c9301935ec20987f9e74f00f7e10689bb65165
wabisabicode <tretyakov@uni-mainz.de>
2025-01-26 21:33:59 +0100

    remove unused import

--- backend/foodgram_backend/api/serializers.py
+++ backend/foodgram_backend/api/serializers.py
@@ -1,5 +1,4 @@
 from django.contrib.auth import get_user_model
-from django.shortcuts import get_object_or_404
 from djoser.serializers import UserCreateSerializer, UserSerializer
 from rest_framework import serializers
 from rest_framework.validators import UniqueTogetherValidator, UniqueValidator
