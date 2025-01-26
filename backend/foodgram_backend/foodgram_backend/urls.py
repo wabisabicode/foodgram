78bd57029aef68b3529f27f756525d4cf3a8dac2
wabisabicode <tretyakov@uni-mainz.de>
2025-01-26 22:20:43 +0100

    try placeholder_view for redirect

--- backend/foodgram_backend/foodgram_backend/urls.py
+++ backend/foodgram_backend/foodgram_backend/urls.py
@@ -3,14 +3,14 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
-from recipe.views import redirect_from_short_url
+from recipe.views import placeholder_view, redirect_from_short_url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls', namespace='api')),
     path('s/<str:hash>/', redirect_from_short_url,
          name='redirect-from-short-link'),
-
+    path('recipes/<int:pk>/', placeholder_view, name='frontend-recipe-detail')
 ]
 
 if settings.DEBUG:
--- backend/foodgram_backend/recipe/views.py
+++ backend/foodgram_backend/recipe/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, redirect
 
 from .models import RecipeShortURL
@@ -5,4 +6,13 @@ from .models import RecipeShortURL
 
 def redirect_from_short_url(request, hash):
     recipe_short_url = get_object_or_404(RecipeShortURL, hash=hash)
-    return redirect('api:recipes-detail', pk=recipe_short_url.recipe.pk)
+    return redirect(
+        'frontend-recipe-detail',
+        kwargs={'pk': recipe_short_url.recipe.pk}
+    )
+
+
+def placeholder_view(request, pk):
+    return HttpResponse(
+        f'This route is handled by the frontend. Recipe ID: {pk}'
+    )
